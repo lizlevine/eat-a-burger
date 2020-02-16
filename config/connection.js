a10c0bc7eb63e6ca26ebdb1f4e875269ea29a34f
Tuscany <lizlevine.rigie@gmail.com>
2020-02-16 15:13:54 -0500

    chore: Add connection to heroku db in production

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@
 var mysql = require('mysql');
 
 // Create the MySQL connection object
-connection = mysql.createConnection({
+var connection = mysql.createConnection(
+    process.env.JAWSDB_URL ||{
     port: 3306,
     host: 'localhost',
     user: 'liz',
@@ -20,40 +21,40 @@ connection.connect(function(err){
 
 // ____________________________________
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
 	// DB is JawsDB on Heroku
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// } else {
 	// DB is local on localhost
-	connection = mysql.createConnection({
-		port: 3306,
-		host: 'localhost',
-		user: 'liz',
-		password: '1234',
-		database: 'burgers_db'
-	})
-};
+// 	connection = mysql.createConnection({
+// 		port: 3306,
+// 		host: 'localhost',
+// 		user: 'liz',
+// 		password: '1234',
+// 		database: 'burgers_db'
+// 	})
+// };
 
   
 // Pull in required dependencies
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 // Create the MySQL connection object
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
+// if (process.env.JAWSDB_URL) {
 	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 	// DB is local on localhost
-	connection = mysql.createConnection({
+	// connection = mysql.createConnection({
 		// port: 3306,
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'burgers_db'
-	})
-};
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: '',
+// 		database: 'burgers_db'
+// 	})
+// };
 
 
 module.exports = connection;
