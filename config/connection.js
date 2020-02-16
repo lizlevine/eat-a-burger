c575162bd835c974febb5ccd3b7858739c9f0559
Tuscany <lizlevine.rigie@gmail.com>
2020-02-15 19:10:08 -0500

    doc: Add sql config

---
+++ config/connection.js
@@ -0,0 +1,45 @@
+  
+// Pull in required dependencies
+var mysql = require('mysql');
+
+// Create the MySQL connection object
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'liz',
+		password: '1234',
+		database: 'burgers_db'
+	})
+};
+
+  
+// Pull in required dependencies
+var mysql = require('mysql');
+
+// Create the MySQL connection object
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	})
+};
+
+// Export connection for ORM use
+connection.connect();
+module.exports = connection;
